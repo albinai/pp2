a9b25cba8d985c6e78395897c31870faab70d8f3
albinai <Albina.13.2.2001@gmail.com>
2019-02-01 17:09:52 +0600

    4

---
+++ week1/Task4/Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); //вводим переменную - количество строк
+
+            for (int i=0; i<n; i++) //настраиваем количество строк
+            {
+                for (int j=0; j<i+1; j++) //количество столбцов
+                {
+                    Console.Write("[*]" + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
