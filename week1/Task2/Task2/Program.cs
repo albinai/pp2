8b9d5db79aafe684988762ae9f3bee9952ea6e56
albinai <Albina.13.2.2001@gmail.com>
2019-02-07 00:36:24 +0600

    2

---
+++ week1/Task2/Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Student
+        {
+        public string name;
+        public string id;
+        public int year = 1;
+
+        public Student(string name, string id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+
+        public void Print()
+        {
+            year++;
+            Console.WriteLine(name + " " + id + " " + year);
+        }
+
+        }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student("Albina", "18BD110909");
+            s.Print();
+            Console.ReadKey();
+        }
+    }
+}
