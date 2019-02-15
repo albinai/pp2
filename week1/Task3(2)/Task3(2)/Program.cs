c898ff1f5422fe39084fd7ad138275bd8cc4d21b
albinai <Albina.13.2.2001@gmail.com>
2019-02-15 14:17:59 +0600

    vtoroi variant

---
+++ week1/Task3(2)/Task3(2)/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string s = Console.ReadLine();
+
+            string[] arr = s.Split();
+
+            int[] m = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                m[i] = int.Parse(arr[i]);
+
+            }
+
+            Doubl(m, n);
+
+        }
+
+        public static void Doubl(int[] m, int n)
+        {
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+
+                list.Add(m[i]);
+                list.Add(m[i]);
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
