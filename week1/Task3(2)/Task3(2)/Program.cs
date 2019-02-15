8e94fd1e461f20d612cdfb74dba200763b8972d6
albinai <Albina.13.2.2001@gmail.com>
2019-02-15 16:36:03 +0600

    dobavila comments

--- week1/Task3(2)/Task3(2)/Program.cs
+++ week1/Task3(2)/Task3(2)/Program.cs
@@ -10,38 +10,38 @@ namespace Task3_2_
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine()); //количество элементов в массиве
 
             string s = Console.ReadLine();
 
-            string[] arr = s.Split();
+            string[] arr = s.Split(); //массив из строк
 
-            int[] m = new int[n];
+            int[] m = new int[n]; //новый массив
 
             for (int i = 0; i < n; i++)
             {
-                m[i] = int.Parse(arr[i]);
+                m[i] = int.Parse(arr[i]); //заполняем массив
 
             }
 
-            Doubl(m, n);
+            Doubl(m, n); //вызываем функцию
 
         }
 
         public static void Doubl(int[] m, int n)
         {
-            List<int> list = new List<int>();
+            List<int> list = new List<int>(); //заводим список
 
             for (int i = 0; i < n; i++)
             {
 
-                list.Add(m[i]);
+                list.Add(m[i]); //добавляем элемент массива в список дважды
                 list.Add(m[i]);
             }
 
             for (int i = 0; i < list.Count; i++)
             {
-                Console.Write(list[i] + " ");
+                Console.Write(list[i] + " "); //выводим на экран
             }
 
             Console.ReadKey();
